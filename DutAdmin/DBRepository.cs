5773205be5aa7896f23fbadde4eef69ff0874460
Yoeshen Govender <yoeshen@gmail.com>
2020-04-17 16:26:44 +0200

    CommitConnected

---
+++ DutAdmin/DBRepository.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using DutAdmin.Models;
+using System.Configuration;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+
+
+
+namespace DutAdmin
+{
+    public static class DBRepository<T> where T : class
+    {
+        private static readonly string DatabaseId = ConfigurationManager.AppSettings["database"];
+        private static readonly string CollectionID = ConfigurationManager.AppSettings["collection"];
+        private static DocumentClient Client;
+
+        public static async Task<T> GetItemAsync(String id,string category)
+        {
+            try
+            {
+
+                Document document = await Client.ReadDocumentAsync(UriFactory.CreateDocumentUri(DatabaseId, CollectionID, id), new RequestOptions { PartitionKey = new PartitionKey(category)});
+                return (T)(dynamic)document;
+            }
+            catch (DocumentClientException e)
+            {
+                if(e.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    return null;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        public static async Task<IEnumerable<T>> GetItemsAsync(Expression<Func<T, bool>> predicate)
+        {
+            IDocumentQuery<T> query = Client.CreateDocumentQuery<T>(
+                UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionID),
+                new FeedOptions { MaxItemCount = -1, EnableCrossPartitionQuery = true })
+                .Where(predicate)
+                .AsDocumentQuery();
+
+            List<T> results = new List<T>();
+            while (query.HasMoreResults)
+            {
+                results.AddRange(await query.ExecuteNextAsync<T>());
+            }
+            return results;
+        }
+
+        public static async Task<Document> CreateItemAsync(T item)
+        {
+            return await Client.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionID), item);
+        }
+
+        public static async Task<Document> UpdateItemAsync(string id, T item)
+        {
+            return await Client.ReplaceDocumentAsync(UriFactory.CreateDocumentUri(DatabaseId, CollectionID, id), item);
+        }
+
+        public static async Task DeleteItemAsync(string id, string category)
+        {
+            await Client.DeleteDocumentAsync(UriFactory.CreateDocumentUri(DatabaseId, CollectionID, id), new RequestOptions { PartitionKey = new PartitionKey(category) });
+        }
+
+        public static void Initialize()
+        {
+            Client = new DocumentClient(new Uri(ConfigurationManager.AppSettings["endpoint"]), ConfigurationManager.AppSettings["authKey"]);
+            CreateDatabaseIfNotExistsAsync().Wait();
+            CreateCollectionIfNotExistsAsync("/category").Wait();
+        }
+
+        public static void Initialize(string endpoint, string authKey)
+        {
+            Client = new DocumentClient(new Uri(endpoint), authKey);
+            CreateDatabaseIfNotExistsAsync().Wait();
+            CreateCollectionIfNotExistsAsync("/category").Wait();
+        }
+
+        public static void Teardown()
+        {
+            DeleteDatabaseAsync().Wait();
+        }
+
+        private static async Task CreateDatabaseIfNotExistsAsync()
+        {
+            try
+            {
+                await Client.ReadDatabaseAsync(UriFactory.CreateDatabaseUri(DatabaseId));
+            }
+            catch (DocumentClientException e)
+            {
+                if (e.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    await Client.CreateDatabaseAsync(new Database { Id = DatabaseId });
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        private static async Task CreateCollectionIfNotExistsAsync(string partitionkey)
+        {
+            try
+            {
+                await Client.ReadDocumentCollectionAsync(UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionID), new RequestOptions { PartitionKey = new PartitionKey(partitionkey) });
+            }
+            catch (DocumentClientException e)
+            {
+                if (e.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    await Client.CreateDocumentCollectionAsync(
+                        UriFactory.CreateDatabaseUri(DatabaseId),
+                        new DocumentCollection
+                        {
+                            Id = CollectionID,
+                            PartitionKey = new PartitionKeyDefinition
+                            {
+                                Paths = new System.Collections.ObjectModel.Collection<string>(new List<string>() { partitionkey })
+                            }
+                        },
+                        new RequestOptions { OfferThroughput = 400 });
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        private static async Task DeleteDatabaseAsync()
+        {
+            await Client.DeleteDatabaseAsync((UriFactory.CreateDatabaseUri(DatabaseId)));
+        }
+    }
+
+
+}
+    
